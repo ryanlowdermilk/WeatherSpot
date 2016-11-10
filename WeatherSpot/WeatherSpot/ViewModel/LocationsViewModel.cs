7b17c5f4b59ee510c4d6aaf2754e80008b2b172a
Ryan Lowdermilk <ryanl@outlook.com>
2016-11-10 17:15:01 -0600

    Created basic LocationsViewModel with Busy support

---
+++ WeatherSpot/WeatherSpot/ViewModel/LocationsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WeatherSpot.ViewModel
+{
+    public class LocationsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        private bool busy;
+
+        public bool IsBusy
+        {
+            get { return busy; }
+            set
+            {
+                busy = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+
+
+
+
+}
