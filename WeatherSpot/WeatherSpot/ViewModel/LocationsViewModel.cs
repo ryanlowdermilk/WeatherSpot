4dd62be9063c1f1d33a3a1bff2ebd7a0e264b65e
Ryan Lowdermilk <ryanl@outlook.com>
2016-11-14 09:31:56 -0600

    Added JSON.net and error handling with alert/popup

--- WeatherSpot/WeatherSpot/ViewModel/LocationsViewModel.cs
+++ WeatherSpot/WeatherSpot/ViewModel/LocationsViewModel.cs
@@ -4,6 +4,11 @@ using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using WeatherSpot.Model;
 using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System;
+using Xamarin.Forms;
+using System.Diagnostics;
 
 namespace WeatherSpot.ViewModel
 {
@@ -37,24 +42,34 @@ namespace WeatherSpot.ViewModel
         {
             if (IsBusy)
                 return;
-            
+
+            Exception error = null;
+
             try
             {
                 IsBusy = true;
 
                 using (var client = new HttpClient())
                 {
-
+                    var json = await client.GetStringAsync("https://demo8782286.mockable.io/locations");
+                    var locations = JsonConvert.DeserializeObject<List<Location>>(json);
+                    Locations.Clear();
+                    foreach (var item in locations)
+                        Locations.Add(item);
                 }
             }
-            catch (System.Exception)
+            catch (Exception ex)
             {
-
+                Debug.WriteLine(ex.StackTrace);
+                error = ex;
             }
             finally
             {
                 IsBusy = false;
             }
+
+            if (error != null)
+                await Application.Current.MainPage.DisplayAlert("Error!", error.Message, "OK");
         }
 
     }
