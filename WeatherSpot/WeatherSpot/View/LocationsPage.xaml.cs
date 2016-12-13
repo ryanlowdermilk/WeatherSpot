368e50a96581e0374d6c4cc5aead772e40d25fef
Ryan Lowdermilk <ryanlo@microsoft.com>
2016-12-13 15:19:15 -0600

    Removed LocationsPage code behind and connected ViewModel via XAML

--- WeatherSpot/WeatherSpot/View/LocationsPage.xaml.cs
+++ WeatherSpot/WeatherSpot/View/LocationsPage.xaml.cs
@@ -16,24 +16,6 @@ namespace WeatherSpot.View
 		public LocationsPage ()
 		{
 			InitializeComponent ();
-            NavigationPage.SetHasNavigationBar(this, false);
-
-            vm = new LocationsViewModel();
-            BindingContext = vm;
-
-            LocationsListView.ItemSelected += LocationsListView_ItemSelected;        
 		}
-
-        private async void LocationsListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-        {
-            var location = e.SelectedItem as Location;
-
-            if (location == null)
-                return;
-
-            await Navigation.PushAsync(new LocationDetailsPage(location));
-
-            LocationsListView.SelectedItem = null;
-        }
     }
 }
